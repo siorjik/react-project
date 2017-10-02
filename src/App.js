a1291492d58a5789367f5773618bf60a2ec3a8fd
Siorjik <serjkaplun@gmail.com>
2017-10-02 09:42:36 +0300

    first commit(add routing, add 4 components)

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import {Router, Route, NavLink, Switch} from 'react-router-dom';
+import createBrowserHistory from "history/createBrowserHistory";
+
+import Groups from "./components/Groups";
+import Coming from "./components/Coming";
+import Products from "./components/Products";
+import Users from "./components/Users";
+import Settings from "./components/Settings";
+
+class App extends Component {
+  render() {
+    return (
+      <Router history={createBrowserHistory()}>
+        <div id='main-wrap'>
+          <header>
+            header
+          </header>
+          <main>
+            <aside>
+              <nav>
+                <ul>
+                  <li><NavLink exact to="/" activeClassName="active">Приход</NavLink></li>
+                  <li><NavLink to="/groups" activeClassName="active">Группы</NavLink></li>
+                  <li><NavLink to="/products" activeClassName="active">Продукты</NavLink></li>
+                  <li><NavLink to="/users" activeClassName="active">Пользователи</NavLink></li>
+                  <li><NavLink to="/settings" activeClassName="active">Настройки</NavLink></li>
+                </ul>
+              </nav>
+            </aside>
+            <div className='content'>
+              <Switch>
+                <Route exact path="/" component={Coming}/>
+                <Route path="/groups" component={Groups}/>
+                <Route path="/products" component={Products}/>
+                <Route path="/users" component={Users}/>
+                <Route path="/settings" component={Settings}/>
+              </Switch>
+            </div>
+          </main>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Coming.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Coming extends Component {
+  render() {
+    return (
+      <div>
+          приход
+      </div>
+    );
+  }
+}
